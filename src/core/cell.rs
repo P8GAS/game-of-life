ad1de810af39e1d00a53fbb98e36520692781461
P8GAS <lucasmplays18@gmail.com>
2026-07-02 13:54:30 +0200

    build: cell implementation

---
+++ src/core/cell.rs
@@ -0,0 +1,29 @@
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum Cell {
+    Alive,
+    Dead,
+}
+
+impl Cell {
+    pub fn is_alive(&self) -> bool {
+        matches!(self, Cell::Alive)
+    }
+
+    pub fn is_dead(&self) -> bool {
+        matches!(self, Cell::Dead)
+    }
+
+    pub fn next_state(&self, neighbors: usize) -> Cell {
+        if self.is_alive() {
+            match neighbors {
+                2 | 3 => Cell::Alive,
+                _ => Cell::Dead,
+            }
+        } else {
+            match neighbors {
+                3 => Cell::Alive,
+                _ => Cell::Dead,
+            }
+        }
+    }
+}
